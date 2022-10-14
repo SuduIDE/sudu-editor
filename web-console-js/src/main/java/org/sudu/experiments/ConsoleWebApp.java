7dc94c1f8c010ef15fcb182014ff0354fdb79603
Kirill Prazdnikov <pkirill@gmail.com>
2022-10-14 12:19:03 +0300

    web console: fetch example (with cors issues)

--- web-console-js/src/main/java/org/sudu/experiments/ConsoleWebApp.java
+++ web-console-js/src/main/java/org/sudu/experiments/ConsoleWebApp.java
@@ -1,6 +1,7 @@
 package org.sudu.experiments;
 
 import org.sudu.experiments.js.JsHelper;
+import org.teavm.jso.ajax.XMLHttpRequest;
 import org.teavm.jso.dom.html.HTMLDocument;
 import org.teavm.jso.dom.html.HTMLInputElement;
 
@@ -12,12 +13,30 @@ public class ConsoleWebApp {
     inputLine.focus();
 
     inputLine.addEventListener("change", e -> {
-      addText("input event :" + inputLine.getValue());
+      String value = inputLine.getValue();
       e.stopPropagation();
       e.preventDefault();
+      addText("input event :" + value);
       inputLine.setValue("");
       inputLine.scrollIntoView();
+      if ("fetch".equals(value)) {
+        fetch();
+      }
     });
+  }
+
+  static void fetch() {
+    XMLHttpRequest request = XMLHttpRequest.create();
+    request.open("GET", "https://github.com/");
+    request.setRequestHeader("Access-Control-Request-Headers", "access-control-allow-origin");
+    request.setRequestHeader("Access-Control-Allow-Origin", "*");
+    request.setOnReadyStateChange(() -> {
+      int readyState = request.getReadyState();
+      addText("request.getReadyState() " + readyState);
+      String headers = request.getAllResponseHeaders();
+      addText(headers);
+    });
+    request.send();
   }
 
   private static void addText(String s) {
