65a37dfce0c8a920132db126e9f436b7cbc050da
Prazdnikov Kirill <pkirill@gmail.com>
2023-06-01 16:43:59 +0300

    revert some recent changes in TextEncoder.java

--- graphics-js/src/main/java/org/sudu/experiments/js/TextEncoder.java
+++ graphics-js/src/main/java/org/sudu/experiments/js/TextEncoder.java
@@ -3,11 +3,17 @@ package org.sudu.experiments.js;
 import org.teavm.jso.JSBody;
 import org.teavm.jso.JSObject;
 import org.teavm.jso.core.JSString;
-import org.teavm.jso.typedarrays.Uint16Array;
+import org.teavm.jso.typedarrays.Uint8Array;
 
 public abstract class TextEncoder implements JSObject {
   @JSBody(script = "return new TextEncoder();")
   public static native TextEncoder create();
+
+  public abstract Uint8Array encode(JSString string);
+
+  public static byte[] toUtf8(JSString string) {
+    return JsMemoryAccess.toByteArray(create().encode(string));
+  }
 
   public static char[] toCharArray(JSString t) {
     int l = t.getLength();
@@ -18,6 +24,4 @@ public abstract class TextEncoder implements JSObject {
     }
     return buffer;
   }
-
-  public abstract Uint16Array encode(JSString string);
 }
