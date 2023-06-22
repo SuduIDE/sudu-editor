46e967ec4ee8e8f414512e5be30b49a43a6b6096
Sergey Loktev <loktevsergay.2003@gmail.com>
2023-06-22 11:38:21 +0300

    Implement revealLine, revealLineInCenter + tests

--- demo-edit-es-module/src/main/java/org/sudu/experiments/JsCodeEditor0.java
+++ demo-edit-es-module/src/main/java/org/sudu/experiments/JsCodeEditor0.java
@@ -164,6 +164,16 @@ public class JsCodeEditor0 implements JsCodeEditor {
   }
 
   @Override
+  public void revealLineInCenter(int line) {
+    editor.revealLineInCenter(line);
+  }
+
+  @Override
+  public void revealLine(int line) {
+    editor.revealLine(line);
+  }
+
+  @Override
   public JsDisposable onDidChangeModel(JsFunctions.Consumer<JsIModelChangedEvent> f) {
     editor.addModelChangeListener(convert(f));
     return JsDisposable.empty();
