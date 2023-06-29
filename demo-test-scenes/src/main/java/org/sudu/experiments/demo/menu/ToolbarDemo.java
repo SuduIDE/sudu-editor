6869dfaaeb3686efa1eeb107fdebfe655df15ea9
Vladislav Kasimov <70969943+rudolf101@users.noreply.github.com>
2023-06-29 20:08:34 +0300

    Add popup menu light scheme

--- demo-test-scenes/src/main/java/org/sudu/experiments/demo/menu/ToolbarDemo.java
+++ demo-test-scenes/src/main/java/org/sudu/experiments/demo/menu/ToolbarDemo.java
@@ -4,6 +4,7 @@ import org.sudu.experiments.Scene0;
 import org.sudu.experiments.SceneApi;
 import org.sudu.experiments.WglGraphics;
 import org.sudu.experiments.demo.Colors;
+import org.sudu.experiments.demo.EditorColorScheme;
 import org.sudu.experiments.demo.SetCursor;
 import org.sudu.experiments.demo.ui.*;
 import org.sudu.experiments.fonts.FontDesk;
@@ -162,7 +163,7 @@ public class ToolbarDemo extends Scene0 implements InputListener {
     System.out.println("onContextMenu");
     if (!popupMenu.isVisible()) {
       popupMenu.display(event.position, items(4),
-              this::onPopupClosed);
+              this::onPopupClosed, EditorColorScheme.darkIdeaColorScheme());
     }
     return true;
   }
