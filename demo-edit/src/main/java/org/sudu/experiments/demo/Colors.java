7f005172934aec487472d364e8258827d8a9b7c6
Vladislav Kasimov <70969943+rudolf101@users.noreply.github.com>
2023-06-29 18:45:19 +0300

    Add FindUsagesWindow light scheme

--- demo-edit/src/main/java/org/sudu/experiments/demo/Colors.java
+++ demo-edit/src/main/java/org/sudu/experiments/demo/Colors.java
@@ -5,6 +5,8 @@ import org.sudu.experiments.demo.ui.ToolbarItemColors;
 import org.sudu.experiments.math.Color;
 import org.sudu.experiments.math.V4f;
 
+import java.util.Objects;
+
 public interface Colors {
 
   V4f scrollBarBody1 = Color.Cvt.fromRGBA(50, 50, 50, 100);
@@ -17,23 +19,36 @@ public interface Colors {
   V4f findUsagesBg = new Color("#3C3F41");
   V4f findUsagesBorder = new Color("#616161");
   V4f findUsagesSelectedBg = new Color("#4B6EAF");
-
+  V4f findusagesBgLight = IdeaCodeColors.Colors.editBgColorLight;
   V4f findUsagesErrorBg = new Color("#781732");
 
-  FindUsagesItemColors findUsagesColors = new FindUsagesItemColors(
-      IdeaCodeColors.Colors.defaultText, IdeaCodeColors.Colors.editNumbersVLine,
-      IdeaCodeColors.Colors.defaultText, Colors.findUsagesBg, Colors.findUsagesSelectedBg
-  );
+  static Color findUsagesColorBorderByScheme(EditorColorScheme scheme) {
+    if (Objects.equals(scheme.editBgColor, IdeaCodeColors.Colors.editBgColor)) {
+      return new Color("#616161");
+    }
+    return new Color("#B9BDC9");
+  }
 
-  FindUsagesItemColors findUsagesColorsContinued = new FindUsagesItemColors(
-      new Color("#CCCCCC"), IdeaCodeColors.Colors.editNumbersVLine,
-      IdeaCodeColors.Colors.defaultText, Colors.findUsagesBg, Colors.findUsagesBg
-  );
+  static FindUsagesItemColors findUsagesColorsByScheme(EditorColorScheme scheme) {
+    if (Objects.equals(scheme.editBgColor, IdeaCodeColors.Colors.editBgColor)) {
+      return FindUsagesItemColors.darkFindUsagesItemColors();
+    }
+    return FindUsagesItemColors.lightFindUsagesItemColors();
+  }
 
-  FindUsagesItemColors findUsagesColorsError = new FindUsagesItemColors(
-      new Color("#BBBBBB"), IdeaCodeColors.Colors.editNumbersVLine,
-      IdeaCodeColors.Colors.defaultText, Colors.findUsagesErrorBg, Colors.findUsagesErrorBg
-  );
+  static FindUsagesItemColors findUsagesColorsContinuedByScheme(EditorColorScheme scheme) {
+    if (Objects.equals(scheme.editBgColor, IdeaCodeColors.Colors.editBgColor)) {
+      return FindUsagesItemColors.darkFindUsagesItemColorsExtraLine();
+    }
+    return FindUsagesItemColors.lightFindUsagesItemColorsExtraLine();
+  }
+
+  static FindUsagesItemColors findUsagesColorsErrorByScheme(EditorColorScheme scheme) {
+    if (Objects.equals(scheme.editBgColor, IdeaCodeColors.Colors.editBgColor)) {
+      return FindUsagesItemColors.darkFindUsagesItemColorsError();
+    }
+    return FindUsagesItemColors.lightFindUsagesItemColorsError();
+  }
 
   ToolbarItemColors popupText = new ToolbarItemColors(
       new Color("#BBBBBB"), toolbarBg, toolbarSelectedBg);
@@ -46,5 +61,4 @@ public interface Colors {
     return new ToolbarItemColors(
         Color.Cvt.fromHSV(Math.random(), 1, 1), toolbarBg, toolbarSelectedBg);
   }
-
 }
