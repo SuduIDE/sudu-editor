b57df123a84b7fa0bea1e93ea9a1e698d41508d5
Vladislav Kasimov <70969943+rudolf101@users.noreply.github.com>
2023-07-12 11:46:48 +0300

    Test refactoring

--- demo-edit-es-module/module/test/ui/testFindUsagesHugeContent.js
+++ demo-edit-es-module/module/test/ui/testFindUsagesHugeContent.js
@@ -2,264 +2,16 @@ import {newEditor, newTextModel} from "../../src/editor.js";
 import {initialTextJava, workerUrl} from "../utils.js";
 
 function test1(editor) {
-    let model = newTextModel(initialTextJava, "java", {path: "WWWWWWWWWWWWWWWWWWWWWWWWWWW.java"})
-    editor.setModel(model)
+    let modelHugeFileName = newTextModel(initialTextJava, "java", {path: "reallyHugeFileNameWWWWWWWWWWWWWWW.java"})
+    let modelNormalFileName = newTextModel(initialTextJava, "java", {path: "somePath.java"})
+    editor.setModel(modelHugeFileName)
 
     editor.registerDefinitionProvider("java",
         {
             provideDefinition(model, position, token) {
-                return [{
-                    uri: model.uri,
-                    range: {
-                        startLineNumber: position.lineNumber + 1,
-                        startColumn: position.column + 5,
-                        endLineNumber: position.lineNumber + 1,
-                        endColumn: position.column + 10
-                    }
-                }, {
-                    uri: model.uri,
-                    range: {
-                        startLineNumber: position.lineNumber - 2,
-                        startColumn: position.column,
-                        endLineNumber: position.lineNumber -  2,
-                        endColumn: position.column
-                    }
-                }, {
-                    uri: model.uri,
-                    range: {
-                        startLineNumber: position.lineNumber + 1,
-                        startColumn: position.column + 5,
-                        endLineNumber: position.lineNumber + 1,
-                        endColumn: position.column + 10
-                    }
-                },
-                    {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber - 2,
-                            startColumn: position.column + 1,
-                            endLineNumber: position.lineNumber - 1,
-                            endColumn: position.column + 2
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber - 2,
-                            startColumn: position.column,
-                            endLineNumber: position.lineNumber -  2,
-                            endColumn: position.column
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        }
-                    },
-                    {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber - 2,
-                            startColumn: position.column + 1,
-                            endLineNumber: position.lineNumber - 1,
-                            endColumn: position.column + 2
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber - 2,
-                            startColumn: position.column,
-                            endLineNumber: position.lineNumber -  2,
-                            endColumn: position.column
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        }
-                    },
-                    {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber - 2,
-                            startColumn: position.column + 1,
-                            endLineNumber: position.lineNumber - 1,
-                            endColumn: position.column + 2
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber - 2,
-                            startColumn: position.column,
-                            endLineNumber: position.lineNumber -  2,
-                            endColumn: position.column
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        }
-                    },
-                    {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber - 2,
-                            startColumn: position.column + 1,
-                            endLineNumber: position.lineNumber - 1,
-                            endColumn: position.column + 2
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber - 2,
-                            startColumn: position.column,
-                            endLineNumber: position.lineNumber -  2,
-                            endColumn: position.column
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        },
-                    },
-                    {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        },
-                    },
-                    {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        },
-                    } , {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber - 2,
-                            startColumn: position.column,
-                            endLineNumber: position.lineNumber -  2,
-                            endColumn: position.column
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        }
-                    },
-                    {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber - 2,
-                            startColumn: position.column + 1,
-                            endLineNumber: position.lineNumber - 1,
-                            endColumn: position.column + 2
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber - 2,
-                            startColumn: position.column,
-                            endLineNumber: position.lineNumber -  2,
-                            endColumn: position.column
-                        }
-                    }, {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        },
-                    },
-                    {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        },
-                    },
-                    {
-                        uri: model.uri,
-                        range: {
-                            startLineNumber: position.lineNumber + 1,
-                            startColumn: position.column + 5,
-                            endLineNumber: position.lineNumber + 1,
-                            endColumn: position.column + 10
-                        },
-                    }
-
-                ]
+                return generateDefinitionData(model, position, 16).concat(
+                    generateDefinitionData(modelNormalFileName, position, 5)
+                );
             }
         }
     );
@@ -272,6 +24,24 @@ function main() {
 
 function run(editor) {
     test1(editor);
+}
+
+function generateDefinitionData(model, position, n) {
+    let definitions = [];
+    for (let i = 0; i < n; i++) {
+            definitions.push(
+                {
+                    uri: model.uri,
+                    range: {
+                        startLineNumber: position.lineNumber - 2 * (i % 2 === 0),
+                        startColumn: position.column,
+                        endLineNumber: position.lineNumber + 2 * (i % 2),
+                        endColumn: position.column
+                    }
+                }
+            )
+    }
+    return definitions
 }
 
 document.addEventListener("DOMContentLoaded", main)
