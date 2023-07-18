6b03bc0fc2290744ea732a61c73bf27b2ddcf846
Sergey Loktev <loktevsergay.2003@gmail.com>
2023-07-18 12:52:55 +0300

    Add doc highlight sample

---
+++ demo-edit-es-module/module/test/providers/testProvideDocHighlight.js
@@ -0,0 +1,39 @@
+import {newEditor, newTextModel} from "../../src/editor.js";
+import {initialTextJava, workerUrl} from "../utils.js";
+
+function test1(editor) {
+    let model = newTextModel(initialTextJava, "java", null)
+    editor.setModel(model)
+    editor.registerDocumentHighlightProvider("java",
+        {
+            provideDocumentHighlights(model, position, token) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() =>
+                        resolve([
+                            {
+                                range: {
+                                    startLineNumber: position.lineNumber + 1,
+                                    startColumn: position.column + 5,
+                                    endLineNumber: position.lineNumber + 1,
+                                    endColumn: position.column + 10
+                                },
+                                kind: 1
+                            }
+                        ]), 3000)
+                })
+            }
+        }
+    )
+}
+
+function main() {
+    newEditor({containerId: "editor", workerUrl: "./../" + workerUrl})
+        .then(run, error => console.error(error));
+}
+
+function run(editor) {
+    test1(editor);
+    console.log("Test successful!");
+}
+
+document.addEventListener("DOMContentLoaded", main)
