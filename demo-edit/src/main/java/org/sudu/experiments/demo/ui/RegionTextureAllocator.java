75c619cd8b13f8b3ecd156fa92c8839da67869b2
Vladislav Kasimov <70969943+rudolf101@users.noreply.github.com>
2023-08-01 12:00:50 +0300

    Add region texture allocator (#39)

---
+++ demo-edit/src/main/java/org/sudu/experiments/demo/ui/RegionTextureAllocator.java
@@ -0,0 +1,23 @@
+package org.sudu.experiments.demo.ui;
+
+import org.sudu.experiments.Canvas;
+import org.sudu.experiments.math.V4f;
+
+import java.util.function.ToIntFunction;
+
+public interface RegionTextureAllocator {
+  int MAX_TEXTURE_SIZE = 2048;
+
+  static ToIntFunction<String> measuring(Canvas canvas) {
+    return text -> (int) (canvas.measureText(text) + 7.f / 8);
+  }
+
+  static ToIntFunction<String> measuringWithWPad(Canvas canvas, float width) {
+    return text -> (int) (canvas.measureText(text) + width * 2 + 7.f / 8);
+  }
+
+  V4f alloc(int width, int height);
+
+  void free(V4f location);
+
+}
--- demo-test-scenes/src/main/java/org/sudu/experiments/demo/menu/FindUsagesDemo.java
+++ demo-test-scenes/src/main/java/org/sudu/experiments/demo/menu/FindUsagesDemo.java
@@ -7,9 +7,14 @@ import org.sudu.experiments.demo.Colors;
 import org.sudu.experiments.demo.IdeaCodeColors;
 import org.sudu.experiments.demo.ui.*;
 import org.sudu.experiments.fonts.FontDesk;
-import org.sudu.experiments.input.*;
+import org.sudu.experiments.input.KeyCode;
+import org.sudu.experiments.input.KeyEvent;
+import org.sudu.experiments.input.MouseEvent;
+import org.sudu.experiments.input.MouseListener;
 import org.sudu.experiments.math.Color;
+import org.sudu.experiments.math.RngHelper;
 import org.sudu.experiments.math.V2i;
+import org.sudu.experiments.math.XorShiftRandom;
 
 public class FindUsagesDemo extends Scene0 implements MouseListener {
 
@@ -42,17 +47,34 @@ public class FindUsagesDemo extends Scene0 implements MouseListener {
     }
   }
 
-  private static FindUsagesItem[] createItems() {
+  // For future add operation
+  @SuppressWarnings("unused")
+  private FindUsagesItem[] createRandomItems(int n) {
     FindUsagesItemBuilder tbb = new FindUsagesItemBuilder();
 
-    addAction(tbb,"main.java","5","private static void foo (...);          ");
-    addAction(tbb,"main.java","25","String foo = \"boo\";                     ");
-    addAction(tbb,"main.java","131","int a = 5;                              ");
-    addAction(tbb,"class.java","176","public class FindTest extend Test {...};");
-    addAction(tbb,"main.java","1234","private static void foo (...);          ");
-    addAction(tbb,"sub.java","4321","private static void foo (...);          ");
-    addAction(tbb,"demo.java","23872","private static void foo (...);          ");
+    XorShiftRandom r = new XorShiftRandom();
+    for (int i = 0; i < n; i++) {
+      String fileName = RngHelper.rngString(r, 5 + (int) (Math.random() * (20 - 5)));
+      String lineNumber = RngHelper.rngString(r, 5 + (int) (Math.random() * (20 - 5)));
+      String codeContent = RngHelper.rngString(r, 5 + (int) (Math.random() * (20 - 5)));
+      addAction(tbb, fileName, lineNumber, codeContent);
+    }
+    return tbb.items();
+  }
 
+  private FindUsagesItem[] createItems() {
+    FindUsagesItemBuilder tbb = new FindUsagesItemBuilder();
+
+    addAction(tbb, "main.java", "5", "private static void foo (...);");
+    addAction(tbb, "main.java", "25", "String foo = \"boo\";");
+    addAction(tbb, "main.java", "131", "int a = 5;");
+    addAction(tbb, "class.java", "176", "public class FindTest extend Test {...};");
+    addAction(tbb, "main.java", "1234", "private static void foo (...);");
+    addAction(tbb, "sub.java", "4321", "private static void foo (...);");
+    addAction(tbb, "demo.java", "23872", "private static void foo (...);");
+    addAction(tbb, "demoWWWWWWWWWWWWWWWWWWWWWWWWW.java", "23872", "private static void foo (...);");
+    addAction(tbb, "demoWWDSADASWDAWDAWDKOAWPDKOPKDPAWKDOADOPKWDOPAKWDOP.java", "23872", "private static void foo (...);");
+    addAction(tbb, "demoWWDSADASWDAWDAWDKOAWPDKOPKDPAWKDOADOPDSAHJDSAKDJSAHDKHDKWDOPAKWDOP.java", "23872", "private static void foo (...);");
     return tbb.items();
   }
 
