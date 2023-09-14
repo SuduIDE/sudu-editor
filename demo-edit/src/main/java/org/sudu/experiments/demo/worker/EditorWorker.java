de7af16c4041c78f1cfd82a25aabb6c0f1f8ce86
Kirill Prazdnikov <pkirill@gmail.com>
2023-09-14 18:15:27 +0300

    format

--- demo-edit/src/main/java/org/sudu/experiments/demo/worker/EditorWorker.java
+++ demo-edit/src/main/java/org/sudu/experiments/demo/worker/EditorWorker.java
@@ -36,7 +36,9 @@ public class EditorWorker {
       case LineParser.PARSE -> LineParser.parse(array(a, 0).chars(), result);
       case JavaParser.PARSE_BYTES_JAVA_VIEWPORT -> JavaParser.parseViewport(array(a, 0).chars(), array(a, 1).ints(), array(a, 2).ints(), result);
       case JavaStructureParser.PARSE_STRUCTURE_JAVA -> JavaStructureParser.parseChars(array(a, 0).chars(), result);
-      case DiffUtils.FIND_DIFFS -> DiffUtils.findDiffs(array(a, 0).chars(), array(a, 1).ints(), array(a, 2).chars(), array(a, 3).ints(), result);
+      case DiffUtils.FIND_DIFFS -> DiffUtils.findDiffs(
+          array(a, 0).chars(), array(a, 1).ints(),
+          array(a, 2).chars(), array(a, 3).ints(), result);
     }
   }
 
