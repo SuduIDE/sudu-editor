04ab05b4138c1a07de463e6412bd82276f3c0146
Vladislav Kasimov <70969943+rudolf101@users.noreply.github.com>
2023-09-18 12:30:42 +0300

    Color measure

--- demo-edit/src/main/java/org/sudu/experiments/demo/ui/colors/DiffColors.java
+++ demo-edit/src/main/java/org/sudu/experiments/demo/ui/colors/DiffColors.java
@@ -7,37 +7,42 @@ public class DiffColors {
   public final V4f deletedBgColor;
   public final V4f insertedBgColor;
   public final V4f editedBgColor;
+  public final V4f editedBgPaleColor;
+
+  DiffColors(
+      V4f deletedBgColor, V4f insertedBgColor,
+      V4f editedBgColor, V4f editedBgPaleColor
+  ) {
+    this.deletedBgColor = deletedBgColor;
+    this.insertedBgColor = insertedBgColor;
+    this.editedBgColor = editedBgColor;
+    this.editedBgPaleColor = editedBgPaleColor;
+  }
 
   public static DiffColors darcula() {
     return new DiffColors(
-        new Color("#CE11EF"),
-        new Color("#63F27D"),
-        new Color("#B35252")
+        new Color("#484A4A"),
+        new Color("#294436"),
+        new Color("#385570"),
+        new Color("#303C47")
     );
   }
 
   public static DiffColors dark() {
     return new DiffColors(
-        new Color("#CE11EF"),
-        new Color("#63F27D"),
-        new Color("#B35252")
+        new Color("#484A4A"),
+        new Color("#294436"),
+        new Color("#385570"),
+        new Color("#283541")
     );
   }
 
   public static DiffColors light() {
     return new DiffColors(
-        new Color("#CE11EF"),
-        new Color("#63F27D"),
-        new Color("#B35252")
+        new Color("#D6D6D6"),
+        new Color("#BEE6BE"),
+        new Color("#C2D8F2"),
+        new Color("#E7EFFA")
     );
-  }
-
-  DiffColors(
-      V4f deletedBgColor, V4f insertedBgColor,
-      V4f editedBgColor
-  ) {
-    this.deletedBgColor = deletedBgColor;
-    this.insertedBgColor = insertedBgColor;
-    this.editedBgColor = editedBgColor;
   }
 }
