4fa287ff3bee192c130460e80b06973678a29f9e
Vladislav Kasimov <70969943+rudolf101@users.noreply.github.com>
2023-09-21 15:10:49 +0300

    Fix mirrored vertical line

--- demo-edit/src/main/java/org/sudu/experiments/demo/EditorComponent.java
+++ demo-edit/src/main/java/org/sudu/experiments/demo/EditorComponent.java
@@ -475,10 +475,6 @@ public class EditorComponent implements Focusable, MouseListener, FontApi {
           sizeTmp, hScrollPos, editorWidth(), tailColor);
     }
 
-    if (hasFocus && caretX >= -caret.width() / 2 && caret.needsPaint(size)) {
-      caret.paint(g, pos);
-    }
-
     // draw bottom 5 invisible lines
     if (renderBlankLines) {
       int nextLine = Math.min(lastLine + 1, docLen);
@@ -491,6 +487,9 @@ public class EditorComponent implements Focusable, MouseListener, FontApi {
 
     if (drawGap) drawGap(firstLine, lastLine, docLen);
 
+    if (hasFocus && caretX >= -caret.width() / 2 && caret.needsPaint(size)) {
+      caret.paint(g, pos);
+    }
 
     layoutScrollbar();
     drawScrollBar();
@@ -518,7 +517,7 @@ public class EditorComponent implements Focusable, MouseListener, FontApi {
 
       if (caretLine == i || currentLineModel != null) {
         vLineSize.x = mirrored
-            ? vLineLeftDelta + scrollBarWidth()
+            ? vLineLeftDelta + scrollBarWidth() + vLineW - xOffset
             : vLineLeftDelta - vLineW - xOffset;
         vLineSize.y = lineHeight;
         int dx2 = mirrored ? 0 : vLineX - vLineLeftDelta + vLineW;
@@ -763,7 +762,9 @@ public class EditorComponent implements Focusable, MouseListener, FontApi {
 
       sizeTmp.y = size.y - yPosition;
       sizeTmp.x = mirrored ? editorWidth() + vLineW : editorWidth();
-      int x = mirrored ? pos.x + vLineLeftDelta + scrollBarWidth() : pos.x + vLineX;
+      int x = mirrored
+          ? pos.x + vLineLeftDelta + scrollBarWidth() + vLineW - xOffset
+          : pos.x + vLineX - xOffset;
       g.drawRect(x, pos.y + yPosition, sizeTmp, colors.editor.bg);
     }
   }
@@ -798,7 +799,9 @@ public class EditorComponent implements Focusable, MouseListener, FontApi {
     vLineSize.x = vLineW;
     int dx1 = mirrored ? size.x - lineNumbersWidth() - vLineW: vLineX - vLineLeftDelta;
     g.drawRect(pos.x + dx1, pos.y, vLineSize, colors.editor.numbersVLine);
-    vLineSize.x = mirrored ? vLineLeftDelta + scrollBarWidth() : vLineLeftDelta - vLineW;
+    vLineSize.x = mirrored
+        ? vLineLeftDelta + scrollBarWidth() + vLineW - xOffset
+        : vLineLeftDelta - vLineW - xOffset;
     int dx2 = mirrored ? 0 : vLineX - vLineLeftDelta + vLineW;
     g.drawRect(pos.x + dx2, pos.y, vLineSize, colors.editor.bg);
   }
