09b8e384472c1b4b6243fcefdafbed4beb29b999
Prazdnikov Kirill <pkirill@gmail.com>
2023-09-26 14:31:02 +0300

    update exmaple

--- demo-edit-es-module/module/samples/loadFromCDN.js
+++ demo-edit-es-module/module/samples/loadFromCDN.js
@@ -1,4 +1,4 @@
-const cdn = "https://cdn.jsdelivr.net/npm/sudu-editor-tmp@0.0.9-beta1"
+const cdn = "https://cdn.jsdelivr.net/npm/sudu-editor-tmp@0.0.9-beta4"
 const editorJs = "/src/editor.js";
 const workerJS = "/src/worker.js"
 
