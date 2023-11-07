152311929d3d8045577371889e3431900ac8ea34
Kirill Prazdnikov <pkirill@gmail.com>
2023-11-07 23:59:26 +0300

    canvas resize bugfix wip

--- graphics-js/src/main/java/org/sudu/experiments/WebWindow.java
+++ graphics-js/src/main/java/org/sudu/experiments/WebWindow.java
@@ -2,11 +2,13 @@ package org.sudu.experiments;
 
 import org.sudu.experiments.js.*;
 import org.sudu.experiments.math.Numbers;
+import org.teavm.jso.JSBody;
 import org.teavm.jso.browser.AnimationFrameCallback;
 import org.teavm.jso.browser.Performance;
 import org.teavm.jso.browser.Window;
 import org.teavm.jso.core.JSError;
 import org.teavm.jso.core.JSObjects;
+import org.teavm.jso.dom.css.CSSStyleDeclaration;
 import org.teavm.jso.dom.events.Event;
 import org.teavm.jso.dom.events.EventListener;
 import org.teavm.jso.dom.events.MessageEvent;
@@ -152,6 +154,11 @@ public class WebWindow implements org.sudu.experiments.Window {
     requestNewFrame();
   }
 
+  @JSBody(
+      params = {"style", "w", "h"},
+      script = "style.width = w + 'px'; style.height = h + 'px';")
+  static native void setStyleWHPx(CSSStyleDeclaration style, double w, double h);
+
   private void onCanvasSizeChanged(int inlineSize, int blockSize) {
     if (1 < 0) {
       JsHelper.consoleInfo("  onCanvasSizeChanged: ", JsHelper.WithId.get(canvasDiv));
@@ -159,9 +166,13 @@ public class WebWindow implements org.sudu.experiments.Window {
       JsHelper.consoleInfo("    blockSize =  ", blockSize);
     }
     eventHandler.setClientRect(inlineSize, blockSize);
+
     mainCanvas.setWidth(inlineSize);
     mainCanvas.setHeight(blockSize);
-
+    double ratio = Window.current().getDevicePixelRatio();
+    double widthPx = (1. / 32. + inlineSize) / ratio;
+    double heightPx = (1. / 32. + blockSize) / ratio;
+    setStyleWHPx(mainCanvas.getStyle(), widthPx, heightPx);
     g.setViewPortAndClientRect(inlineSize, blockSize);
     scene.onResize(g.clientRect, devicePixelRatio());
     scene.paint();
