77199372b34bb94817c13c2d47e2fb777b76372a
Dmitry Ivanov <korifey@gmail.com>
2023-11-08 17:47:33 +0300

    Merge + changed initial text

--- demo-edit-es-module/module/samples/activity.js
+++ demo-edit-es-module/module/samples/activity.js
@@ -2,14 +2,30 @@ import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.mi
 mermaid.initialize({ startOnLoad: false });
 
 const initialText = "activity {\n" +
-    "  select{A1,B1};\n" +
-    "  random(2){A1,B1,C1};\n" +
-    "  random {X1,Y1,Z1};\n" +
-    "  A2;\n" +
-    "  repeat(2){A4};\n" +
-    "  if ({A1->A2}) {A4};\n" +
-    "  if (A4) {A4};\n" +
-    "  else {A5}\n" +
+    "  select{A1,B1, C1};\n" +
+    "  A1;\n" +
+    "  select{\n" +
+    "    sequence {\n" +
+    "      select{(A1)E1, F1};\n" +
+    "      G1;\n" +
+    "      H1;\n" +
+    "      select{\n" +
+    "        (E1) X1, \n" +
+    "        Y1, \n" +
+    "        (A1 and (B1 or C1)) Z1\n" +
+    "      }\n" +
+    "    };\n" +
+    "    sequence {\n" +
+    "      G1;\n" +
+    "      select{M1, N1};\n" +
+    "      H1;\n" +
+    "      select{\n" +
+    "       ({A1->A1} and M1) A1;\n" +
+    "       B1;\n" +
+    "       ({A1,G1} and N1) C1\n" +
+    "      }\n" +
+    "    }\n" +
+    "  }  \n" +
     "}";
 
 //CDN
