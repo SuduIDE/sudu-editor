389658942da4bf16d09c11a01dcd058320dc3b77
pertsevpv <pertsevpv@yandex.ru>
2023-11-22 11:38:34 +0300

    Optimized ExprRefNode

--- parser/src/main/java/org/sudu/experiments/parser/common/graph/node/ref/ExprRefNode.java
+++ parser/src/main/java/org/sudu/experiments/parser/common/graph/node/ref/ExprRefNode.java
@@ -1,5 +1,6 @@
 package org.sudu.experiments.parser.common.graph.node.ref;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExprRefNode extends RefNode {
@@ -13,7 +14,14 @@ public class ExprRefNode extends RefNode {
   public ExprRefNode(List<RefNode> refNodes, String type) {
     super(null, null, -1);
     if (refNodes.isEmpty()) throw new IllegalArgumentException("Empty Expression");
-    this.refNodes = refNodes;
+    this.refNodes = new ArrayList<>();
+    for (var expr: refNodes) {
+      if (expr instanceof ExprRefNode exprRef) {
+        this.refNodes.addAll(exprRef.refNodes);
+      } else {
+        this.refNodes.add(expr);
+      }
+    }
     this.type = type;
   }
 
