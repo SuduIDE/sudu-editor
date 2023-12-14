17d5b334389ebc2c30a522f9f9368220a33d3a99
Prazdnikov Kirill <pkirill@gmail.com>
2023-12-14 18:33:37 +0300

    adjust waveline parameters for very small scale factors

--- demo-edit/src/main/java/org/sudu/experiments/editor/UnderlineConstants.java
+++ demo-edit/src/main/java/org/sudu/experiments/editor/UnderlineConstants.java
@@ -4,28 +4,34 @@ import org.sudu.experiments.math.Numbers;
 import org.sudu.experiments.math.V4f;
 
 interface UnderlineConstants {
-  float basePow = 3.f / 8;
+  float amplitude = .75f;
+  float extend = 1.25f;
+  float pow = 3.f / 8;
+
+  float minExtend = extend * 3 / 3;
   float maxPow  = 1.f / 2;
   float maxPowScale = 4;
-  double scalePow = Numbers.log(Numbers.log(maxPow, basePow), maxPowScale);
+  double scalePow = Numbers.log(Numbers.log(maxPow, pow), maxPowScale);
 
-  // x = 2 * Pi / frequency
+  // x = 2 * Pi * frequency
   // y = amplitude
-  // z = width / 2
+  // z = extend = width / 2
   // z = power: basePow - brighter, 1.0 - no change
   static void sinParamsDefault(V4f result) {
-    result.set((float)Math.PI / 3, 1, 1.25f, basePow);
+    result.set((float)Math.PI / 3, amplitude, extend, pow);
   }
 
   static void scaleSinParams(V4f arg, float scale, V4f result) {
+    // to reduce noise and improve readability
+    // we use scale = .25 when scale below .5
+    float fScale = scale < 0.5f ? .25f : scale;
     // pow ranges from basePow to maxPow
     // basePow when scale == 1, maxPow when scale == maxPowScale
-    float pow = scale >= maxPowScale ? maxPow
-        : (float) Math.pow(arg.w, Math.pow(scale, scalePow));
+    float pow = fScale >= maxPowScale ? maxPow
+        : (float) Math.pow(arg.w, Math.pow(fScale, scalePow));
     result.set(
-        arg.x / scale,
-        arg.y * scale,
-        arg.z * scale, pow
+        arg.x / fScale, arg.y * fScale,
+        Math.max(arg.z * fScale, minExtend), pow
     );
   }
 
