ff8145d40832ff0ae4d506ed7e291471f5a51c10
pertsevpv <pertsevpv@yandex.ru>
2024-02-19 11:24:30 +0300

    html iterative parsing support

---
+++ parser/src/main/java/org/sudu/experiments/parser/html/parser/HtmlIterativeParser.java
@@ -0,0 +1,10 @@
+package org.sudu.experiments.parser.html.parser;
+
+import org.sudu.experiments.parser.common.base.FullIntervalParser;
+import org.sudu.experiments.parser.html.gen.HTMLParser;
+
+public class HtmlIterativeParser extends FullIntervalParser<HTMLParser> {
+  public HtmlIterativeParser() {
+    super(new HtmlFullParser());
+  }
+}
