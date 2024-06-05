1bc42dcc300b7e7f8a48ab1a9739150aa709586b
Prazdnikov Kirill <pkirill@gmail.com>
2024-06-05 16:43:58 +0300

    fs test

--- filediff-node-module/module/samples/fsSample0.mjs
+++ filediff-node-module/module/samples/fsSample0.mjs
@@ -9,14 +9,6 @@ function isDirectory(dirName) {
   return stats ? stats.isDirectory() : false;
 }
 
-const args = process.argv;
-console.log(args);
-
-var dirname = args[2];
-
-console.log("arg = ", dirname);
-console.log("isDirectory = ", isDirectory(dirname));
-
 function traverseDir(dirName) {
   console.log("dirName: ", dirName);
   const result = fs.readdirSync(dirName);
@@ -27,7 +19,7 @@ function traverseDir(dirName) {
   //   console.log("entry: " + entry);
   // }
 
-  const readBuffer = new Uint8Array((1024|0) * 64);
+  const readBuffer = new Uint8Array(1024 * 64 | 0);
 
   for (let i = 0; i < result.length; i++) {
     const child = dirName + path.sep + result[i];
@@ -47,8 +39,25 @@ function traverseDir(dirName) {
   }
 }
 
-if (isDirectory(dirname)) {
-  traverseDir(dirname);
+const start = performance.now()
+
+const args = process.argv;
+
+if (args.length < 3) {
+  console.log("usage node script folder")
 } else {
-  console.error(dirname + " is not a directory")
+
+  const dirname = args[2];
+
+  console.log("args[2] = ", dirname);
+  console.log("isDirectory = ", isDirectory(dirname));
+
+  if (isDirectory(dirname)) {
+    traverseDir(dirname);
+  } else {
+    console.error(dirname + " is not a directory")
+  }
+
+  let dT = (performance.now() - start);
+  console.log("dT = ", dT);
 }
