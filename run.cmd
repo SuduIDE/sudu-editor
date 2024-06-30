61c53b085a12b0987f2566457b98c6a5eead9bfb
Kirill Prazdnikov <pkirill@gmail.com>
2024-06-30 22:28:50 +0300

    update scripts

--- run.cmd
+++ run.cmd
@@ -8,8 +8,8 @@ set dir2=D:\Github\llvm-project-b
 
 set dir3=D:\chromium-main
 
-set dir1=%dir3%
-set dir2=%dir3%
+set dir1=%dir%
+set dir2=%dir%
 
 set cp=%dir%\demo-edit-jvm\target\classes;%dir%\graphics-jvm\target\classes;%dir%\graphics\target\classes;%dir%\graphics-common\target\classes;%dir%\angle-dll\target\classes;%dir%\demo-edit\target\classes;%dir%\parser\target\classes;%dir%\parser-common\target\classes;C:\Users\Kirill\.m2\repository\org\antlr\antlr4-runtime-teavm-compatible\4.13.2-SNAPSHOT\antlr4-runtime-teavm-compatible-4.13.2-SNAPSHOT.jar;%dir%\diff-model\target\classes;%dir%\parser-activity\target\classes;%dir%\fonts\target\classes;%dir%\codicon\target\classes;%dir%\demo-test-scenes\target\classes
 
@@ -19,15 +19,13 @@ set nativeExe=demo-edit-jvm\target\FolderDiffTestJvm.exe
 
 mkdir logs
 
-node %mjs% %dir1% %dir2% content > logs\dry_n1 2>&1
-node %mjs% %dir1% %dir2% content sync > logs\dry_n1s 2>&1
-node %mjs% %dir1% %dir2% content > logs\dry_n2 2>&1
-node %mjs% %dir1% %dir2% content sync > logs\dry_n2s 2>&1
-rem %jjj% -cp %cp% org.sudu.experiments.FolderDiffTestJvm %dir1% %dir2% content > logs\dry_j1 2>&1
-rem %jjj% -cp %cp% org.sudu.experiments.FolderDiffTestJvm %dir1% %dir2% content sync > logs\dry_js1 2>&1
-rem %jjj% -cp %cp% org.sudu.experiments.FolderDiffTestJvm %dir1% %dir2% content > logs\dry_j2 2>&1
-rem %jjj% -cp %cp% org.sudu.experiments.FolderDiffTestJvm %dir1% %dir2% content sync> logs\dry_js2 2>&1
-rem %nativeExe% %dir1% %dir2% content > logs\dry_graal 2>&1
+call :CallNode logs\dry_n0 %dir1% %dir2%
+rem call :CallNode logs\dry_n1 %dir1% %dir2%
+rem call :CallNode logs\dry_n1s %dir1% %dir2% sync
+
+call :CallJava logs\dry_j0 %dir1% %dir2% sync
+
+rem call :CallGraalNative logs\dry_g0 %dir1% %dir2% sync
 
 exit
 
@@ -37,3 +35,14 @@ FOR %%i IN (0,1,2,3) DO (
   %nativeExe% %dir1% %dir2% content > logs\graalrun%%i 2>&1
 )
 
+:CallNode
+node %mjs% %~2 %~3 content %~4 > %~1 2>&1
+EXIT /B 0
+
+:CallGraalNative
+%nativeExe% %~2 %~3 content %~4 > %~1 2>&1
+EXIT /B 0
+
+:CallJava
+%jjj% -cp %cp% org.sudu.experiments.FolderDiffTestJvm %~2 %~3 content %~4 > %~1 2>&1
+EXIT /B 0
