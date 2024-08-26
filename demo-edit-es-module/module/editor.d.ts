55943f4d1220dcc1f10e74fc69d62d65d2a31fc1
Anatoly Nikitin <niktolik@gmail.com>
2024-08-26 15:20:55 +0300

    Add comment in `IEvent` interface

--- demo-edit-es-module/module/editor.d.ts
+++ demo-edit-es-module/module/editor.d.ts
@@ -33,6 +33,13 @@ interface IDisposable {
 }
 
 interface IEvent<T> {
+    /**
+     * Registers disposable event callback
+     *
+     * When event is fired, a new value V will be delivered to all listeners that were registered at the moment when the value V processing started.
+     * It means that if listener A disposes listener B during processing of value V, listener B will still receive value V.
+     * @param listener callback to be called when event is fired
+     */
     (listener: (e: T) => any): IDisposable;
 }
 
