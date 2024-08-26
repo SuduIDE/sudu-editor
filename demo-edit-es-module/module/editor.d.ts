52fd7066a764666f38d9fe5831317d715ab1648b
Anatoly Nikitin <niktolik@gmail.com>
2024-08-26 14:42:50 +0300

    Replace `getIsReady()`  with `isReady()`

--- demo-edit-es-module/module/editor.d.ts
+++ demo-edit-es-module/module/editor.d.ts
@@ -223,7 +223,7 @@ interface EditView extends ICodeEditor, IDisposable {}
 export interface IFolderDiff extends EditorBase, HasTheme, Focusable {}
 
 export interface FolderDiffView extends IFolderDiff, IDisposable {
-    getIsReady(): boolean
+    isReady(): boolean
     onIsReadyChanged: IEvent<boolean>
 }
 
