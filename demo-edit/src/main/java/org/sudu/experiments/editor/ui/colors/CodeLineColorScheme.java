0b1ebb70e92457b3416ebc5213b22feb1b34bccf
Prazdnikov Kirill <pkirill@gmail.com>
2024-09-12 14:08:45 +0300

    minor formatting

--- demo-edit/src/main/java/org/sudu/experiments/editor/ui/colors/CodeLineColorScheme.java
+++ demo-edit/src/main/java/org/sudu/experiments/editor/ui/colors/CodeLineColorScheme.java
@@ -1,9 +1,6 @@
 package org.sudu.experiments.editor.ui.colors;
 
-import org.sudu.experiments.fonts.Fonts;
 import org.sudu.experiments.math.Color;
-import org.sudu.experiments.parser.ParserConstants;
-import org.sudu.experiments.ui.UiFont;
 
 public class CodeLineColorScheme {
   public final Color currentLineBg;
@@ -14,17 +11,17 @@ public class CodeLineColorScheme {
   public final CodeElementColor[] codeElement;
   public final DiffColors diff;
 
-    public CodeLineColorScheme(
-        Color currentLineBg, Color definitionBg, Color usageBg,
-        Color selectionBg, Color defaultBg,
-        CodeElementColor[] codeElement, DiffColors diff
-    ) {
-        this.currentLineBg = currentLineBg;
-        this.definitionBg = definitionBg;
-        this.usageBg = usageBg;
-        this.selectionBg = selectionBg;
-        this.defaultBg = defaultBg;
-        this.codeElement = codeElement;
-        this.diff = diff;
-    }
+  public CodeLineColorScheme(
+      Color currentLineBg, Color definitionBg, Color usageBg,
+      Color selectionBg, Color defaultBg,
+      CodeElementColor[] codeElement, DiffColors diff
+  ) {
+    this.currentLineBg = currentLineBg;
+    this.definitionBg = definitionBg;
+    this.usageBg = usageBg;
+    this.selectionBg = selectionBg;
+    this.defaultBg = defaultBg;
+    this.codeElement = codeElement;
+    this.diff = diff;
+  }
 }
