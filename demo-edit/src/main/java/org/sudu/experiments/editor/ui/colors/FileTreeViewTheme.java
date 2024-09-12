6ecec3b796ba47e74ad6c2101568dbf05f310347
Prazdnikov Kirill <pkirill@gmail.com>
2024-09-12 14:09:45 +0300

    minor formatting

--- demo-edit/src/main/java/org/sudu/experiments/editor/ui/colors/FileTreeViewTheme.java
+++ demo-edit/src/main/java/org/sudu/experiments/editor/ui/colors/FileTreeViewTheme.java
@@ -9,32 +9,35 @@ public class FileTreeViewTheme {
   public final Color inactiveSelectedBg;
   public final Color hoveredBg;
 
-  public FileTreeViewTheme(Color bg, Color selectedBg, Color selectedText, Color inactiveSelectedBg, Color hoveredBg) {
+  public FileTreeViewTheme(
+      Color bg, Color selectedBg, Color selectedText,
+      Color inactiveSelectedBg, Color hoveredBg
+  ) {
     this.bg = bg;
     this.selectedBg = selectedBg;
-      this.selectedText = selectedText;
-      this.inactiveSelectedBg = inactiveSelectedBg;
-      this.hoveredBg = hoveredBg;
+    this.selectedText = selectedText;
+    this.inactiveSelectedBg = inactiveSelectedBg;
+    this.hoveredBg = hoveredBg;
   }
 
   public static FileTreeViewTheme darculaIdea() {
     return new FileTreeViewTheme(
-            new Color("#2B2D30"), new Color("#04395e"),
-            new Color("#FFFFFF"),
-            new Color("#37373d"), new Color("#ffffff11"));
+        new Color("#2B2D30"), new Color("#04395e"),
+        new Color("#FFFFFF"),
+        new Color("#37373d"), new Color("#ffffff11"));
   }
 
   public static FileTreeViewTheme darkIdea() {
     return new FileTreeViewTheme(
-            new Color("#3C3F41"), new Color("#04395e"),
-            new Color("#FFFFFF"),
-            new Color("#37373d"), new Color("#ffffff11"));
+        new Color("#3C3F41"), new Color("#04395e"),
+        new Color("#FFFFFF"),
+        new Color("#37373d"), new Color("#ffffff11"));
   }
 
   public static FileTreeViewTheme lightIdea() {
     return new FileTreeViewTheme(
-            new Color("#F7F8FA"), new Color("#0060c0"),
-            new Color("#FFFFFF"),
-            new Color("#e4e6f1"), new Color("#00000011"));
+        new Color("#F7F8FA"), new Color("#0060c0"),
+        new Color("#FFFFFF"),
+        new Color("#e4e6f1"), new Color("#00000011"));
   }
 }
