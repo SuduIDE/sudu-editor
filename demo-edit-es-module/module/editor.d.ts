7c58022dcf167ad7d4d487f121c32f13cab52cb2
Anatoly Nikitin <niktolik@gmail.com>
2024-09-27 17:33:26 +0300

    Tabs formatting

--- demo-edit-es-module/module/editor.d.ts
+++ demo-edit-es-module/module/editor.d.ts
@@ -4,63 +4,63 @@
 import { Channel, IDisposable } from "./common";
 
 export {
-    Channel, Message, setLogLevel, setLogOutput, newRemoteChannelTest, LogLevel, ChannelTestApi, IDisposable
+  Channel, Message, setLogLevel, setLogOutput, newRemoteChannelTest, LogLevel, ChannelTestApi, IDisposable
 } from './common';
 
 export interface EditArgs {
-    containerId: string
+  containerId: string
 
-    // default value for workerUrl is "worker.js"
-    workerUrl?: string | URL
+  // default value for workerUrl is "worker.js"
+  workerUrl?: string | URL
 
-    theme?: Theme
+  theme?: Theme
 
-    readonly?: boolean
+  readonly?: boolean
 
-    // number of worker threads for parsing and resolve
-    // default: 2
-    numThreads?: number
+  // number of worker threads for parsing and resolve
+  // default: 2
+  numThreads?: number
 
-    codiconUrl?: string
+  codiconUrl?: string
 }
 
 interface Uri {
-    scheme?: string,
-    authority?: string,
-    path?: string
+  scheme?: string,
+  authority?: string,
+  path?: string
 }
 
 interface IEvent<T> {
-    /**
-     * Registers disposable event callback
-     *
-     * When event is fired, a new value V will be delivered to all listeners that were registered at the moment when the value V processing started.
-     * It means that if listener A disposes listener B during processing of value V, listener B will still receive value V.
-     * @param listener callback to be called when event is fired
-     */
-    (listener: (e: T) => any): IDisposable;
+  /**
+   * Registers disposable event callback
+   *
+   * When event is fired, a new value V will be delivered to all listeners that were registered at the moment when the value V processing started.
+   * It means that if listener A disposes listener B during processing of value V, listener B will still receive value V.
+   * @param listener callback to be called when event is fired
+   */
+  (listener: (e: T) => any): IDisposable;
 }
 
 /**
  * An event describing that an editor has had its model reset (i.e. `editor.setModel()`).
  */
 export interface IModelChangedEvent {
-    /**
-     * The `uri` of the previous model or null.
-     */
-    readonly oldModelUrl: Uri | null;
-    /**
-     * The `uri` of the new model or null.
-     */
-    readonly newModelUrl: Uri | null;
+  /**
+   * The `uri` of the previous model or null.
+   */
+  readonly oldModelUrl: Uri | null;
+  /**
+   * The `uri` of the new model or null.
+   */
+  readonly newModelUrl: Uri | null;
 }
 
 export interface ITextModel extends IDisposable {
-    language?: string
-    uri?: Uri
-    getOffsetAt(position: IPosition): number
-    getPositionAt(offset: number): IPosition
-    getText(): string
+  language?: string
+  uri?: Uri
+  getOffsetAt(position: IPosition): number
+  getPositionAt(offset: number): IPosition
+  getText(): string
 }
 
 type ProviderValue<T> = T | undefined | null;
@@ -70,84 +70,84 @@ type ProviderResult<T> = ProviderValue<T> | Promise<ProviderValue<T>>;
 //      1st line has number 1,
 //      first char column is 1, 1st glyph is between column 1 and column 2
 interface IPosition {
-    column: number,
-    lineNumber: number
+  column: number,
+  lineNumber: number
 }
 
 // numeration starts from 1
 interface IRange {
-    endColumn: number,
-    endLineNumber: number,
-    startColumn: number,
-    startLineNumber: number
+  endColumn: number,
+  endLineNumber: number,
+  startColumn: number,
+  startLineNumber: number
 }
 
 interface ICancellationToken {
-    isCancellationRequested: boolean
+  isCancellationRequested: boolean
 }
 
 interface ILocation {
-    range: IRange,
-    uri: Uri
+  range: IRange,
+  uri: Uri
 }
 
 export enum DocumentHighlightKind {
-    Text = 0,
-    Read = 1,
-    Write = 2
+  Text = 0,
+  Read = 1,
+  Write = 2
 }
 
 interface IDocumentHighlight {
-    range: IRange,
-    kind?: DocumentHighlightKind
+  range: IRange,
+  kind?: DocumentHighlightKind
 }
 
 interface IDefinitionProvider {
-    provideDefinition(
-        model: ITextModel,
-        position: IPosition,
-        token: ICancellationToken
-    ): ProviderResult<ILocation[]>
+  provideDefinition(
+    model: ITextModel,
+    position: IPosition,
+    token: ICancellationToken
+  ): ProviderResult<ILocation[]>
 }
 
 interface IDeclarationProvider {
-    provideDeclaration(
-        model: ITextModel,
-        position: IPosition,
-        token: ICancellationToken
-    ): ProviderResult<ILocation[]>
+  provideDeclaration(
+    model: ITextModel,
+    position: IPosition,
+    token: ICancellationToken
+  ): ProviderResult<ILocation[]>
 }
 
 interface IReferenceProvider {
-    provideReferences(
-        model: ITextModel,
-        position: IPosition,
-        context: { includeDeclaration: boolean },
-        token: ICancellationToken
-    ): ProviderResult<ILocation[]>
+  provideReferences(
+    model: ITextModel,
+    position: IPosition,
+    context: { includeDeclaration: boolean },
+    token: ICancellationToken
+  ): ProviderResult<ILocation[]>
 }
 
 interface IDocumentHighlightProvider {
-    provideDocumentHighlights(
-        model: ITextModel,
-        position: IPosition,
-        token: ICancellationToken
-    ): ProviderResult<IDocumentHighlight[]>
+  provideDocumentHighlights(
+    model: ITextModel,
+    position: IPosition,
+    token: ICancellationToken
+  ): ProviderResult<IDocumentHighlight[]>
 }
 
 type SelectionOrPosition = IRange | IPosition;
 
 interface ICodeEditorOpener {
-    openCodeEditor(
-        source: IEditorView,
-        resource: Uri,
-        selectionOrPosition?: SelectionOrPosition
-    ): boolean | Promise<boolean>;
+  openCodeEditor(
+    source: IEditorView,
+    resource: Uri,
+    selectionOrPosition?: SelectionOrPosition
+  ): boolean | Promise<boolean>;
 }
 
 interface ILanguageFilter {
-    language?: string
-    scheme?: string
+  language?: string
+  scheme?: string
 }
 
 type LanguageSelector = string | ILanguageFilter | Array<string | ILanguageFilter>;
@@ -164,173 +164,173 @@ type LanguageSelector = string | ILanguageFilter | Array<string | ILanguageFilte
 // }
 
 export const enum ThemeColor {
-    TreeViewBackground = 0,
-    DefaultForeground = 1,
-    SelectedItemBackground = 2,
-    SelectedItemForeground = 3,
-    HoveredItemBackground = 4,
-    InactiveSelectionBackground = 5,
-    ChangedItemBackground = 6
+  TreeViewBackground = 0,
+  DefaultForeground = 1,
+  SelectedItemBackground = 2,
+  SelectedItemForeground = 3,
+  HoveredItemBackground = 4,
+  InactiveSelectionBackground = 5,
+  ChangedItemBackground = 6
 }
 
 export type BaseTheme = 'dark' | 'light' | 'darcula';
 
 export type Theme = {
-    [color in ThemeColor]?: string;
+  [color in ThemeColor]?: string;
 } & {
-    baseTheme: BaseTheme;
+  baseTheme: BaseTheme;
 } | BaseTheme;
 
 interface HasTheme {
-    setFontFamily(fontFamily: string): void,
+  setFontFamily(fontFamily: string): void,
 
-    setFontSize(fontSize: number): void,
+  setFontSize(fontSize: number): void,
 
-    setTheme(theme: Theme): void
+  setTheme(theme: Theme): void
 }
 
 interface Focusable {
-    focus(): void
+  focus(): void
 }
 
 interface View {
-    disconnectFromDom(): void
-    reconnectToDom(containerId?: string): void
+  disconnectFromDom(): void
+  reconnectToDom(containerId?: string): void
 }
 
 export interface TwoPanelDiff {
-    setReadonly(leftReadonly: boolean, rightReadonly: boolean): void
+  setReadonly(leftReadonly: boolean, rightReadonly: boolean): void
 }
 
 export interface IEditorView extends View, HasTheme, Focusable {
-    setText(text: string): void,
+  setText(text: string): void,
 
-    getText(): string,
+  getText(): string,
 
-    setPosition(position: IPosition): void
+  setPosition(position: IPosition): void
 
-    getPosition(): IPosition
+  getPosition(): IPosition
 
-    getModel(): ITextModel
+  getModel(): ITextModel
 
-    registerDefinitionProvider(languageSelector: LanguageSelector, provider: IDefinitionProvider): IDisposable
-    registerDeclarationProvider(languageSelector: LanguageSelector, provider: IDeclarationProvider): IDisposable
-    registerReferenceProvider(languageSelector: LanguageSelector, provider: IReferenceProvider): IDisposable
+  registerDefinitionProvider(languageSelector: LanguageSelector, provider: IDefinitionProvider): IDisposable
+  registerDeclarationProvider(languageSelector: LanguageSelector, provider: IDeclarationProvider): IDisposable
+  registerReferenceProvider(languageSelector: LanguageSelector, provider: IReferenceProvider): IDisposable
 
-    registerDocumentHighlightProvider(languageSelector: LanguageSelector, provider: IDocumentHighlightProvider): IDisposable
+  registerDocumentHighlightProvider(languageSelector: LanguageSelector, provider: IDocumentHighlightProvider): IDisposable
 
-    registerEditorOpener(opener: ICodeEditorOpener): IDisposable
+  registerEditorOpener(opener: ICodeEditorOpener): IDisposable
 
-    revealLineInCenter(line: number): void
+  revealLineInCenter(line: number): void
 
-    revealLine(line: number): void
+  revealLine(line: number): void
 
-    revealPosition(position: IPosition): void
+  revealPosition(position: IPosition): void
 }
 
 interface EditorView extends IEditorView, IDisposable {
-    setReadonly(flag: boolean): void
+  setReadonly(flag: boolean): void
 
-    setModel(model: ITextModel): void
+  setModel(model: ITextModel): void
 
-    onDidChangeModel: IEvent<IModelChangedEvent>
+  onDidChangeModel: IEvent<IModelChangedEvent>
 }
 
 export interface IFileDiffView extends View, HasTheme, Focusable, TwoPanelDiff {
-    getLeftModel(): ITextModel,
+  getLeftModel(): ITextModel,
 
-    getRightModel(): ITextModel,
+  getRightModel(): ITextModel,
 }
 
 interface FileDiffView extends IFileDiffView, IDisposable {
-    setLeftModel(model: ITextModel): void,
+  setLeftModel(model: ITextModel): void,
 
-    setRightModel(model: ITextModel): void,
+  setRightModel(model: ITextModel): void,
 }
 
 export interface IFolderDiffView extends View, HasTheme, TwoPanelDiff, Focusable {
-    isReady(): boolean
-    onReadyChanged: IEvent<boolean>
+  isReady(): boolean
+  onReadyChanged: IEvent<boolean>
 }
 
 export interface FolderDiffView extends IFolderDiffView, IDisposable {
 }
 
 export interface FolderDiffSelection {
-    // relativePath does not include root folder.
-    // For root folder relativePath is empty
-    relativePath: string
-    isLeft: boolean
-    isFolder: boolean
+  // relativePath does not include root folder.
+  // For root folder relativePath is empty
+  relativePath: string
+  isLeft: boolean
+  isFolder: boolean
 
-    // whether selected element exists only in current diff side
-    isOrphan: boolean
+  // whether selected element exists only in current diff side
+  isOrphan: boolean
 }
 
 export interface FileDiffSelection {
 }
 
 export interface ViewController {
-    getViewType(): 'folderDiff' | 'fileDiff' | 'editor'
-    getSelection(): FolderDiffSelection | FileDiffSelection | undefined
+  getViewType(): 'folderDiff' | 'fileDiff' | 'editor'
+  getSelection(): FolderDiffSelection | FileDiffSelection | undefined
 
-    canNavigateUp(): boolean
-    navigateUp(): void
+  canNavigateUp(): boolean
+  navigateUp(): void
 
-    canNavigateDown(): boolean
-    navigateDown(): void
+  canNavigateDown(): boolean
+  navigateDown(): void
 
-    refresh(): void
+  refresh(): void
 }
 
 export enum DiffType {
-    Same = 0, Added = 1, Deleted = 2, Modified = 3
+  Same = 0, Added = 1, Deleted = 2, Modified = 3
 }
 
 export interface FolderDiffViewController extends ViewController {
-    getViewType(): 'folderDiff'
-    getSelection(): FolderDiffSelection | undefined
+  getViewType(): 'folderDiff'
+  getSelection(): FolderDiffSelection | undefined
 
-    getDiffFilter(): DiffType[]
-    applyDiffFilter(filters: DiffType[]): void
+  getDiffFilter(): DiffType[]
+  applyDiffFilter(filters: DiffType[]): void
 }
 
 export interface FileDiffViewController extends ViewController {
-    getViewType(): 'fileDiff'
-    getSelection(): FileDiffSelection | undefined
+  getViewType(): 'fileDiff'
+  getSelection(): FileDiffSelection | undefined
 }
 
 export interface EditorViewController extends ViewController {
-    getViewType(): 'editor'
-    getSelection(): undefined
+  getViewType(): 'editor'
+  getSelection(): undefined
 }
 
 export interface ExternalFileOpener {
-    // All paths are absolute
-    openFileDiff(leftPath: string, rightPath: string): void
-    openEditor(path: string): void
+  // All paths are absolute
+  openFileDiff(leftPath: string, rightPath: string): void
+  openEditor(path: string): void
 }
 
 export interface RemoteFolderDiffView extends IFolderDiffView, IDisposable {
-    getState(): any
-    applyState(state: any): void
-    getController(): FolderDiffViewController | FileDiffViewController | EditorViewController;
-    onControllerUpdate: IEvent<FolderDiffViewController | FileDiffViewController | EditorViewController>
-    setExternalFileOpener(opener: ExternalFileOpener | null): void
+  getState(): any
+  applyState(state: any): void
+  getController(): FolderDiffViewController | FileDiffViewController | EditorViewController;
+  onControllerUpdate: IEvent<FolderDiffViewController | FileDiffViewController | EditorViewController>
+  setExternalFileOpener(opener: ExternalFileOpener | null): void
 }
 
 export interface RemoteFileDiffView extends IFileDiffView, IDisposable {
-    getState(): any
-    applyState(state: any): void
-    getController(): FileDiffViewController;
-    onControllerUpdate: IEvent<FileDiffViewController>
+  getState(): any
+  applyState(state: any): void
+  getController(): FileDiffViewController;
+  onControllerUpdate: IEvent<FileDiffViewController>
 }
 
 export interface RemoteEditorView extends IEditorView, IDisposable {
-    getState(): any
-    applyState(state: any): void
-    getController(): EditorViewController;
-    onControllerUpdate: IEvent<EditorViewController>
+  getState(): any
+  applyState(state: any): void
+  getController(): EditorViewController;
+  onControllerUpdate: IEvent<EditorViewController>
 }
 
 export function newTextModel(text: string, language?: string, uri?: Uri): ITextModel
