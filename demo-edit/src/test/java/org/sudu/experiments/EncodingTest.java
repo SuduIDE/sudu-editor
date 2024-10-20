45ed69ef42d06b51149ec6df5838c6e16adee18d
Kirill Prazdnikov <pkirill@gmail.com>
2024-10-20 15:59:30 +0300

    GB2312 & Utf-8 detection

---
+++ demo-edit/src/test/java/org/sudu/experiments/EncodingTest.java
@@ -0,0 +1,24 @@
+package org.sudu.experiments;
+
+import org.sudu.experiments.editor.FileEncoding;
+
+public class EncodingTest {
+
+  public static void main(String[] args) {
+    testFile("code.c");
+    testFile("README.md");
+    testFile("code-utf.c");
+    testFile("README-utf.md");
+    testFile("UTF-8-demo.html");
+  }
+
+  private static void testFile(String filename) {
+    System.out.println("filename = " + filename);
+    byte[] s = ReadResource.readFileBytes(filename, EncodingTest.class);
+    System.out.println("  data = " + s);
+    boolean utf8 = FileEncoding.isUtf8(s, true);
+    System.out.println("  utf8 = " + utf8);
+    boolean gb2312 = FileEncoding.isGB2312_EUC_CN(s);
+    System.out.println("  gb2312 = " + gb2312);
+  }
+}
