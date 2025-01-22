6843ddc1991625d2471481b4c462843852c62a7a
Kirill Prazdnikov <pkirill@gmail.com>
2025-01-22 11:55:30 +0300

    wip

--- filediff-node-module/src/main/java/org/sudu/experiments/js/node/JsSftpClient.java
+++ filediff-node-module/src/main/java/org/sudu/experiments/js/node/JsSftpClient.java
@@ -1,7 +1,20 @@
 package org.sudu.experiments.js.node;
 
+import org.sudu.experiments.js.JsArrayReader;
+import org.sudu.experiments.js.JsFunctions.BiConsumer;
 import org.teavm.jso.JSObject;
+import org.teavm.jso.JSProperty;
+import org.teavm.jso.core.JSError;
+import org.teavm.jso.core.JSString;
 
 public interface JsSftpClient extends JSObject {
-
+  interface DirEntry extends JSObject {
+    @JSProperty
+    JSString getFilename();
+    @JSProperty
+    JSObject getAttrs();
+  }
+  void readdir(
+      JSString path,
+      BiConsumer<JSError, JsArrayReader<DirEntry>> callback);
 }
--- filediff-node-module/src/main/java/org/sudu/experiments/js/node/SshDirectoryHandle.java
+++ filediff-node-module/src/main/java/org/sudu/experiments/js/node/SshDirectoryHandle.java
@@ -1,6 +1,11 @@
 package org.sudu.experiments.js.node;
 
 import org.sudu.experiments.JsFileInputSsh.JaSshCredentials;
+import org.sudu.experiments.LoggingJs;
+import org.sudu.experiments.SshPool;
+import org.sudu.experiments.js.JsHelper;
+import org.sudu.experiments.js.Promise;
+import org.teavm.jso.core.JSError;
 import org.teavm.jso.core.JSString;
 
 import java.util.function.Consumer;
@@ -20,6 +25,16 @@ public class SshDirectoryHandle extends NodeDirectoryHandle0 {
 
   @Override
   public void read(Reader reader) {
+    Promise<SshPool.Record> connected = SshPool.connect(credentials);
+    connected.then(
+        connection -> {
+          JsSftpClient sftp = connection.getSftp();
+
+        },
+        error -> LoggingJs.error(
+            JsHelper.concat("", JsHelper.getMessage(error))
+        )
+    );
     reader.onComplete();
   }
 
