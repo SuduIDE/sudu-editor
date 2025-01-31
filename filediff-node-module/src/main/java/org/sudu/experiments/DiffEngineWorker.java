840993637425f4f21500b1539af886d2397b388b
Kirill Prazdnikov <pkirill@gmail.com>
2025-01-31 10:40:15 +0300

    minor cleanup

--- filediff-node-module/src/main/java/org/sudu/experiments/DiffEngineWorker.java
+++ filediff-node-module/src/main/java/org/sudu/experiments/DiffEngineWorker.java
@@ -5,16 +5,9 @@ import org.sudu.experiments.editor.worker.ThreadId;
 import org.sudu.experiments.js.NodeWorker;
 import org.sudu.experiments.js.node.NodeWorkersBridge;
 
-import java.util.function.Consumer;
-
 public class DiffEngineWorker {
-  static class Executor {
-    public static void execute(String method, Object[] a, Consumer<Object[]> onResult) {
-      FileDiffWorker.execute(method, a, onResult);
-    }
-  }
   public static void main(String[] args) {
     ThreadId.id = "DiffEngineWorker.main";
-    NodeWorker.workerMain(Executor::execute, new NodeWorkersBridge());
+    NodeWorker.workerMain(FileDiffWorker::execute, new NodeWorkersBridge());
   }
 }
