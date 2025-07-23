693e80c0bed960db3bdf21fb11e9bfaf3f4cc8d6
Prazdnikov Kirill <pkirill@gmail.com>
2025-07-23 12:41:42 +0300

    fix test

--- demo-edit-es-module/module/test/diffTool/testMultipleDiffTools.js
+++ demo-edit-es-module/module/test/diffTool/testMultipleDiffTools.js
@@ -1,15 +1,15 @@
-import {newTextModel, newCodeDiff} from "../../src/editor.js";
+import {newTextModel, newFileDiff} from "../../src/editor.js";
 import {initialTextJava, workerUrl} from "../utils.js";
 
 const editorArgs = {containerId: "editor", workerUrl: "./../" + workerUrl};
 
 function main() {
-    newCodeDiff(editorArgs).then(
+  newFileDiff(editorArgs).then(
         diff1 => {
             let model = newTextModel(initialTextJava, "java", "urlNew");
             diff1.setLeftModel(model)
             diff1.setReadonly(true)
-            newCodeDiff(editorArgs).then(
+          newFileDiff(editorArgs).then(
                 diff2 => {
                     diff2.setRightModel(model)
                     diff2.setReadonly(true)
