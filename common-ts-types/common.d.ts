551a16d6b1a1c4a07f6cb0e4d15cb62a889ff415
Prazdnikov Kirill <pkirill@gmail.com>
2025-12-11 14:48:33 +0300

    common ts types

--- common-ts-types/common.d.ts
+++ common-ts-types/common.d.ts
@@ -1,20 +1,18 @@
-
 export interface IDisposable {
-    dispose(): void
+  dispose(): void
 }
 
 export const enum LogLevel {
-    OFF = 0,
-    FATAL = 1,
-    ERROR = 2,
-    WARN = 3,
-    INFO = 4,
-    DEBUG = 5,
-    TRACE = 6
+  OFF = 0,
+  FATAL = 1,
+  ERROR = 2,
+  WARN = 3,
+  INFO = 4,
+  DEBUG = 5,
+  TRACE = 6
 }
 
 export function setLogLevel(logLevel: LogLevel): void;
 
 export function setLogOutput(logHandler: (logLevel: LogLevel, text: string)=>void): void;
-
 
